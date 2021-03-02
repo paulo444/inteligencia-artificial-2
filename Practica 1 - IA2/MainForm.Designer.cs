0b48fba60ad417aad70f76b025fb39777c34873c
Paulo Celis <pauloxyo@gmail.com>
2021-03-02 16:46:46 -0600

    Graphic Interface

--- Practica 1 - IA2/MainForm.Designer.cs
+++ Practica 1 - IA2/MainForm.Designer.cs
@@ -36,12 +36,182 @@ namespace Practica_1___IA_2
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.startPerceptron = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.epochNumber = new System.Windows.Forms.Label();
+			this.dataGrid1 = new System.Windows.Forms.DataGrid();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(344, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(189, 44);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Práctica 1 - IA2";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(80, 56);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(375, 370);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// startPerceptron
+			// 
+			this.startPerceptron.Location = new System.Drawing.Point(217, 441);
+			this.startPerceptron.Name = "startPerceptron";
+			this.startPerceptron.Size = new System.Drawing.Size(98, 23);
+			this.startPerceptron.TabIndex = 2;
+			this.startPerceptron.Text = "Perceptrón";
+			this.startPerceptron.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(635, 56);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(92, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "M:";
+			this.label2.Click += new System.EventHandler(this.Label2Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(733, 56);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(116, 22);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(733, 84);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(116, 22);
+			this.textBox2.TabIndex = 6;
+			this.textBox2.TextChanged += new System.EventHandler(this.TextBox2TextChanged);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(635, 84);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(92, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "#Epochmax:";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(674, 199);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(105, 23);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Resultados";
+			// 
+			// epochNumber
+			// 
+			this.epochNumber.Location = new System.Drawing.Point(610, 233);
+			this.epochNumber.Name = "epochNumber";
+			this.epochNumber.Size = new System.Drawing.Size(143, 23);
+			this.epochNumber.TabIndex = 8;
+			this.epochNumber.Text = "#Epochs: ";
+			// 
+			// dataGrid1
+			// 
+			this.dataGrid1.DataMember = "";
+			this.dataGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+			this.dataGrid1.Location = new System.Drawing.Point(599, 281);
+			this.dataGrid1.Name = "dataGrid1";
+			this.dataGrid1.Size = new System.Drawing.Size(253, 183);
+			this.dataGrid1.TabIndex = 9;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(652, 143);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Inicializa";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(733, 117);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(116, 23);
+			this.label5.TabIndex = 11;
+			this.label5.Text = "W0: ";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(733, 142);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(116, 23);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "W1: ";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(733, 166);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(116, 23);
+			this.label7.TabIndex = 13;
+			this.label7.Text = "W2: ";
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Practica 1 - IA 2";
+			this.ClientSize = new System.Drawing.Size(906, 514);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGrid1);
+			this.Controls.Add(this.epochNumber);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.startPerceptron);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
 			this.Name = "MainForm";
+			this.Text = "Practica 1 - IA 2";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DataGrid dataGrid1;
+		private System.Windows.Forms.Label epochNumber;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button startPerceptron;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
 	}
 }
--- Practica 1 - IA2/MainForm.cs
+++ Practica 1 - IA2/MainForm.cs
@@ -20,14 +20,7 @@ namespace Practica_1___IA_2
 	{
 		public MainForm()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
 		}
 	}
 }
